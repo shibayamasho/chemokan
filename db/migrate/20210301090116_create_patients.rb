ed14b40976764287cc9fda8173f60c297f5a2f91
shibayamasho <men.in.boss2@gmail.com>
2021-03-01 18:05:59 +0900

    マイグレーションファイルを記述

--- db/migrate/20210301090116_create_patients.rb
+++ db/migrate/20210301090116_create_patients.rb
@@ -1,7 +1,10 @@
 class CreatePatients < ActiveRecord::Migration[6.0]
   def change
     create_table :patients do |t|
-
+      t.string  :name,      null: false
+      t.integer :gender_id, null: false
+      t.date    :birthday,  null: false
+      t.text    :text
       t.timestamps
     end
   end
