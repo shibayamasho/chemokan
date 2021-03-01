a718f66a8041de48c4d5f8c5fcf440f6d2e5d9be
shibayamasho <men.in.boss2@gmail.com>
2021-03-01 16:02:55 +0900

    user FactoryBotを記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :user do
-    
+    uid                   { Faker::Lorem.characters(min_length: 8, max_length: 9) }
+    name                  { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.free_email }
+    password              { Faker::Internet.password(min_length: 6, max_length: 30) }
+    password_confirmation { password }
   end
 end
