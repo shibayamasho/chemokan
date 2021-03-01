91def3f14aa856d855ceb189641c0fc6eb996b68
shibayamasho <men.in.boss2@gmail.com>
2021-03-01 18:26:43 +0900

    ActiveHash 性別を生成・記述

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,7 @@
 class Patient < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :gender
+
   with_options presence: true do
     NAME_REGEX = /\A[A-Z]+\z/
     validates :name, format: { with: NAME_REGEX }
