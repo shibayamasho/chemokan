2e57d10315d592d89668cd21514442e137d49de1
shibayamasho <men.in.boss2@gmail.com>
2021-03-01 13:00:41 +0900

    userモデルにバリデーションを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :name
+
+    UID_REGEX = /\A[a-z0-9]+\z/
+    validates :uid, format: { with: UID_REGEX },
+                    niqueness: true,
+                    length: { minimum: 8 }
+  end
 end
