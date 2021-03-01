6158d9c8de18e5ad6ffa8cc635ed25224f67e53d
shibayamasho <men.in.boss2@gmail.com>
2021-03-01 18:18:15 +0900

    バリデーションを記述

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,2 +1,8 @@
 class Patient < ApplicationRecord
+  with_options presence: true do
+    NAME_REGEX = /\A[A-Z]+\z/
+    validates :name, format: { with: NAME_REGEX }
+    validates :gender_id, numericality: { other_than: 1 }
+    validates :birthday
+  end
 end
