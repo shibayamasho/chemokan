341fd9f38283e1bc04050f7746591481c201ce3b
shibayamasho <men.in.boss2@gmail.com>
2021-03-02 19:36:25 +0900

    diseaseモデルファイルを記述

--- app/models/disease.rb
+++ app/models/disease.rb
@@ -1,2 +1,12 @@
 class Disease < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+
+  with_options presence: true, numericality: { other_than: 1 } do
+    validates :cancer_id
+    validates :other_id
+    validates :purpose_id
+  end
+
+  # has_many :regimens, through: disease_regimens
+  # has_many :treatments
 end
