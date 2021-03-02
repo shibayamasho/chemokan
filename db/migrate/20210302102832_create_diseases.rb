9503a52e2cb327af1c0d4ae7336e4616cf6a2ed5
shibayamasho <men.in.boss2@gmail.com>
2021-03-02 19:31:14 +0900

    マイグレーションファイルを記述

--- db/migrate/20210302102832_create_diseases.rb
+++ db/migrate/20210302102832_create_diseases.rb
@@ -1,7 +1,9 @@
 class CreateDiseases < ActiveRecord::Migration[6.0]
   def change
     create_table :diseases do |t|
-
+      t.integer :cancer_id,  null: false
+      t.integer :other_id,   null: false
+      t.integer :purpose_id, null: false
       t.timestamps
     end
   end
