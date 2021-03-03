5fb859820ab40480b170fbf4484990cb77ec3c85
shibayamasho <men.in.boss2@gmail.com>
2021-03-03 21:49:33 +0900

    Purposeを修正

--- app/models/purpose.rb
+++ app/models/purpose.rb
@@ -3,7 +3,7 @@ class Purpose < ActiveHash::Base
     { id: 1, name: '--' },
     { id: 2, name: '術後補助化学療法' },
     { id: 3, name: '術前化学療法' },
-    { id: 4, name: '切除不能の進行・再発がん' }
+    { id: 4, name: '切除不能の進行・再発がん' },
     { id: 5, name: 'その他' }
   ]
 
