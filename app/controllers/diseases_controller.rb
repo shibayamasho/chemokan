50de14d5c5c40b4c7e40dc66f745c0acf0f2683e
shibayamasho <men.in.boss2@gmail.com>
2021-03-03 19:22:31 +0900

    diseases_controller.rbを記述

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,2 +1,15 @@
 class DiseasesController < ApplicationController
+  def index
+    @diseases = Disease.all
+  end
+
+  def create
+    disease = Disease.create(disease_params)
+    redirect_to diseases_path
+  end
+
+  private
+  def disease_params
+    params.require(:disease).permit(:cancer_id, :other_id, :purpose_id)
+  end
 end
