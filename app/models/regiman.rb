71cc4a5226d3992f44015c5a4cd98abb05a3fecb
shibayamasho <men.in.boss2@gmail.com>
2021-03-04 15:47:32 +0900

    regimenモデルを記述

--- app/models/regiman.rb
+++ app/models/regiman.rb
@@ -1,2 +1,31 @@
 class Regiman < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :emetic
+  belongs_to :unit
+
+  with_options presence: true do
+    validates :name
+    validates :period
+    validates :medicinen_name1
+    validates :abbreviation1
+    validates :dose1
+    validates :schedule1
+    with_options numericality: { other_than: 1 } do
+      validates :emetic_id
+      validates :unit_id1
+    end
+  end
+
+  with_options numericality: { other_than: 1 } do
+    validates :unit_id2
+    validates :unit_id3
+    validates :unit_id4
+    validates :unit_id5
+    validates :unit_id6
+    validates :unit_id7
+    validates :unit_id8
+  end
+
+  # has_many :diseases, through: disease_regimens
+  # has_many :treatments
 end
