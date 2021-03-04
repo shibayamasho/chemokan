3cf9ad256ef4938e5e90f2675e6d31a67d7d3e97
shibayamasho <men.in.boss2@gmail.com>
2021-03-04 15:02:53 +0900

    マイグレーションを実行

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_02_102832) do
+ActiveRecord::Schema.define(version: 2021_03_04_055011) do
 
   create_table "diseases", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "cancer_id", null: false
@@ -25,6 +25,60 @@ ActiveRecord::Schema.define(version: 2021_03_02_102832) do
     t.integer "gender_id", null: false
     t.date "birthday", null: false
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "regimen", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.integer "period", null: false
+    t.integer "emetic_id", null: false
+    t.string "nk1"
+    t.string "dex"
+    t.string "h1blockler"
+    t.string "h2blockler"
+    t.string "other_medicine"
+    t.text "text"
+    t.string "medicinen_name1", null: false
+    t.string "medicinen_name2"
+    t.string "medicinen_name3"
+    t.string "medicinen_name4"
+    t.string "medicinen_name5"
+    t.string "medicinen_name6"
+    t.string "medicinen_name7"
+    t.string "medicinen_name8"
+    t.string "abbreviation1", null: false
+    t.string "abbreviation2"
+    t.string "abbreviation3"
+    t.string "abbreviation4"
+    t.string "abbreviation5"
+    t.string "abbreviation6"
+    t.string "abbreviation7"
+    t.string "abbreviation8"
+    t.float "dose1", null: false
+    t.float "dose2"
+    t.float "dose3"
+    t.float "dose4"
+    t.float "dose5"
+    t.float "dose6"
+    t.float "dose7"
+    t.float "dose8"
+    t.integer "unit_id1", null: false
+    t.integer "unit_id2"
+    t.integer "unit_id3"
+    t.integer "unit_id4"
+    t.integer "unit_id5"
+    t.integer "unit_id6"
+    t.integer "unit_id7"
+    t.integer "unit_id8"
+    t.string "schedule1", null: false
+    t.string "schedule2"
+    t.string "schedule3"
+    t.string "schedule4"
+    t.string "schedule5"
+    t.string "schedule6"
+    t.string "schedule7"
+    t.string "schedule8"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
