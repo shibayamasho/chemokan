0412ad4a3679a790a1dd7c4995641a6553ff4396
shibayamasho <men.in.boss2@gmail.com>
2021-03-05 19:16:04 +0900

    Update plan.rb

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,7 +1,14 @@
 class Plan < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :emetic
-  belongs_to :unit
+  belongs_to :unit1
+  belongs_to :unit2
+  belongs_to :unit3
+  belongs_to :unit4
+  belongs_to :unit5
+  belongs_to :unit6
+  belongs_to :unit7
+  belongs_to :unit8
 
   with_options presence: true do
     validates :name, uniqueness: true
@@ -12,18 +19,18 @@ class Plan < ApplicationRecord
     validates :schedule1
     with_options numericality: { other_than: 1 } do
       validates :emetic_id
-      validates :unit_id1
+      validates :unit1_id
     end
   end
 
   with_options numericality: true do
-    validates :unit_id2
-    validates :unit_id3
-    validates :unit_id4
-    validates :unit_id5
-    validates :unit_id6
-    validates :unit_id7
-    validates :unit_id8
+    validates :unit2_id
+    validates :unit3_id
+    validates :unit4_id
+    validates :unit5_id
+    validates :unit6_id
+    validates :unit7_id
+    validates :unit8_id
   end
 
   # has_many :diseases, through: disease_plans
