4821b409b6d2357b6c457705cf81f2e239980c73
shibayamasho <men.in.boss2@gmail.com>
2021-03-05 19:16:45 +0900

    ActiveHash Unitを1~8まで生成（仕方なく。後日修正）

--- app/models/unit5.rb
+++ app/models/unit5.rb
@@ -1,4 +1,4 @@
-class Unit < ActiveHash::Base
+class Unit5 < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
     { id: 2, name: 'mg/㎡' },
---
+++ app/models/unit6.rb
@@ -0,0 +1,15 @@
+class Unit6 < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: 'mg/㎡' },
+    { id: 3, name: 'mg/kg' },
+    { id: 4, name: 'mg/body' },
+    { id: 5, name: 'μg/body' },
+    { id: 6, name: 'AUC' },
+    { id: 7, name: 'UI/㎡' },
+    { id: 8, name: 'KE/body' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :plans
+end
