569e6f85b2fce7a9fd20329138bdbcdc943c3f62
shibayamasho <men.in.boss2@gmail.com>
2021-03-08 18:20:13 +0900

    Update diseases_controller.rb

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -3,20 +3,20 @@ class DiseasesController < ApplicationController
   def create
     @disease = Disease.new(disease_params)
     if @disease.save
-      redirect_to plan_path(@plan.id)
+      redirect_to plan_path(params[:plan_id])
     else
-      render plan_path(@plan.id)
+      render plan_path(params[:plan_id])
     end
   end
 
   def destroy
     @disease = Disease.find(params[:id])
     @disease.destroy
-    redirect_to plan_path(@plan.id)
+    redirect_to plan_path(params[:plan_id])
   end
 
   private
   def disease_params
-    params.require(:disease).permit(:cancer_id, :other_id, :purpose_id).merge(plan_id: @plan.id)
+    params.require(:disease).permit(:cancer_id, :other_id, :purpose_id) .merge(plan_id: params[:plan_id])
   end
 end
