54efb8d9f385e30e2033d6baa917f9e1e33fd0b4
shibayamasho <men.in.boss2@gmail.com>
2021-03-08 16:47:38 +0900

    diseasesコントローラーを修正

--- app/controllers/diseases_controller.rb
+++ app/controllers/diseases_controller.rb
@@ -1,45 +1,22 @@
 class DiseasesController < ApplicationController
-  before_action :set_disease, only: [:edit, :update, :destroy]
-
-  def index
-    @diseases = Disease.all.order(:cancer_id, :other_id, :purpose_id)
-    @disease = Disease.new
-  end
 
   def create
     @disease = Disease.new(disease_params)
     if @disease.save
-      redirect_to diseases_path
+      redirect_to plan_path(@plan.id)
     else
-      @diseases = Disease.all.order(:cancer_id, :other_id, :purpose_id)
-      render :index
-    end
-  end
-
-  def edit
-  end
-
-  def update
-    if @disease.update(disease_params)
-      redirect_to diseases_path
-    else
-      render :edit
+      render plan_path(@plan.id)
     end
   end
 
   def destroy
+    @disease = Disease.find(params[:id])
     @disease.destroy
-    redirect_to diseases_path
+    redirect_to plan_path(@plan.id)
   end
-
 
   private
-
-  def set_disease
-    @disease = Disease.find(params[:id])
-  end
-
   def disease_params
-    params.require(:disease).permit(:cancer_id, :other_id, :purpose_id)
+    params.require(:disease).permit(:cancer_id, :other_id, :purpose_id).merge(plan_id: @plan.id)
   end
 end
