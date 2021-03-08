59a06a9bcf852ea159e8c4a6223b456bb16dea53
shibayamasho <men.in.boss2@gmail.com>
2021-03-08 21:24:21 +0900

    treatment model を記述

--- app/models/treatment.rb
+++ app/models/treatment.rb
@@ -1,2 +1,20 @@
 class Treatment < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :in_out
+
+  with_options presence: true do
+    validates :data
+    validates :in_out_id, numericality: { other_than: 1 }
+    with_options numericality: true do
+      validates :course
+      validates :day
+    end
+    validates :mesuring_date
+    validates :height
+    validates :weight
+  end
+  
+  belongs_to :disease
+  belongs_to :plan
+  belongs_to :patient
 end
