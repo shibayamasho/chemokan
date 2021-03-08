6f14e7ceadad5b69f6caf852dd9618920ef40652
shibayamasho <men.in.boss2@gmail.com>
2021-03-08 19:35:46 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "patients#index"
-  resources :treatments, only: [:index]
+  #resources :treatments
   resources :patients
   resources :plans do
     resources :diseases, only: [:create, :destroy]
