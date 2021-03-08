7a4933582275a017a6172b5f21ad20b352bb16e9
shibayamasho <men.in.boss2@gmail.com>
2021-03-08 19:13:22 +0900

    Update cancer.rb

--- app/models/cancer.rb
+++ app/models/cancer.rb
@@ -11,7 +11,17 @@ class Cancer < ActiveHash::Base
     { id: 9, name: '胆道がん' },
     { id: 10, name: '腎がん' },
     { id: 11, name: '結腸・直腸がん' },
-    { id: 12, name: 'その他' }
+    { id: 12, name: '前立腺がん' },
+    { id: 13, name: '尿路上皮がん' },
+    { id: 14, name: '急性骨髄性白血病' },
+    { id: 15, name: '急性リンパ性白血病' },
+    { id: 16, name: '慢性骨髄性白血病' },
+    { id: 17, name: '慢性リンパ性白血病' },
+    { id: 18, name: 'ホジキンリンパ腫' },
+    { id: 19, name: '非ホジキンリンパ腫' },
+    { id: 20, name: '多発性骨髄腫' },
+    { id: 21, name: '骨髄異形成症候群' },
+    { id: 22, name: 'その他' }
   ]
 
   include ActiveHash::Associations
