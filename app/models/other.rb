10b556b5309159ea021be54f05960dd06b6528f3
shibayamasho <men.in.boss2@gmail.com>
2021-03-08 20:40:55 +0900

    Update other.rb

--- app/models/other.rb
+++ app/models/other.rb
@@ -5,7 +5,13 @@ class Other < ActiveHash::Base
     { id: 3, name: 'HER2(-)' },
     { id: 4, name: 'EGFRmu(+)' },
     { id: 5, name: 'EGFRmu(-)' },
-    { id: 6, name: 'その他' }
+    { id: 6, name: 'Luminal A' },
+    { id: 7, name: 'Luminal B, HER2(-)' },
+    { id: 8, name: 'Luminal B, HER2(+)' },
+    { id: 9, name: 'Triple Negative' },
+    { id: 10, name: 'Driver Mutation(+)' },
+    { id: 11, name: 'Driver Mutation(-)' },
+    { id: 12, name: 'その他' }
   ]
 
   include ActiveHash::Associations
