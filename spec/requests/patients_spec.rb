3fe84e6235eb20f235dd7c6084259332dbc79f8a
shibayamasho <men.in.boss2@gmail.com>
2021-03-17 14:11:02 +0900

    patients indexアクション

--- spec/requests/patients_spec.rb
+++ spec/requests/patients_spec.rb
@@ -1,10 +1,53 @@
 require 'rails_helper'
+describe PatientsController, type: :request do
+#RSpec.describe "Patients", type: :request do
 
-RSpec.describe "Patients", type: :request do
-  describe "GET /patients" do
-    it "works! (now write some real specs)" do
-      get patients_index_path
-      expect(response).to have_http_status(200)
+  # コントローラー単体テストを実行する際は、basic認証とauthenticate_user!をコメントアウトしておくこと。
+
+  before do
+    @patient = FactoryBot.create(:patient)
+  end
+
+  describe "GET #index" do
+    it "indexアクションにリクエストすると、正常にレスポンスが返ってくる" do
+      get root_path
+      expect(response.status).to eq 200
+    end
+
+    it "indexアクションにリクエストすると、レスポンスにログアウトのボタンが存在する" do
+      get root_path
+      expect(response.body).to include('ログアウト')
+    end
+
+    it "indexアクションにリクエストすると、レスポンスにレジメン一覧へのリンクが存在する" do
+      get root_path
+      binding.pry
+      expect(response.body).to include('レジメン一覧')
+    end
+
+    it "indexアクションにリクエストすると、レスポンスに患者一覧が存在する" do
+      get root_path
+      expect(response.body).to include('登録患者一覧')
+    end
+
+    it "indexアクションにリクエストすると、レスポンスに登録した患者名が存在する" do
+      get root_path
+      expect(response.body).to include(@patient.name)
+    end
+
+    it "indexアクションにリクエストすると、レスポンスに登録した患者の性別が存在する" do
+      get root_path
+      expect(response.body).to include(@patient.gender.name)
+    end
+
+    it "indexアクションにリクエストすると、レスポンスに患者新規登録ページへのリンクが存在する" do
+      get root_path
+      expect(response.body).to include('患者新規登録')
+    end
+
+    it "indexアクションにリクエストすると、レスポンスに患者検索フォームが存在する" do
+      get root_path
+      expect(response.body).to include('患者名で検索する')
     end
   end
 end
